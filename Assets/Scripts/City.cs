42152792bd09f28725c3d55efc1adcb348f60c2f
Murilo Bezerra <78811958+Murilazuo@users.noreply.github.com>
2023-05-17 15:35:09 -0300

    sprites

--- Assets/Scripts/City.cs
+++ Assets/Scripts/City.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class City : MonoBehaviour
 {
+    [SerializeField] Sprite[] sprites;
+    [SerializeField] SpriteRenderer spr;
+
     public Vector2 Position { get => transform.position; }
     private void Start()
     {
         SetScale(CitySizeSeter.size);
+        spr.sprite = sprites[Random.Range(0, sprites.Length)];
     }
     void SetScale(float size)
     {
